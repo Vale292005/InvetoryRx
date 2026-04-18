ec13efb7516d38ffd63143a66137969694fa7bce
Vale292005 <valeria.florezp@uqvirtual.edu.co>
2026-04-18 08:45:54 -0500

    correccion de errores en dashboard-produt10

--- src/components/FormCrearUsuario.vue
+++ src/components/FormCrearUsuario.vue
@@ -2,6 +2,9 @@
 import { useCrearUsuario } from '../Composable/useCrearUser';
 import CustomInput from '@/components/CustomInput.vue';
 import CustomButton from '@/components/CustomButton.vue';
+import Accordion from '@/components/Accordion.vue';
+
+const opciones=['ADMIN', 'USER', 'MANAGER', 'SALES'];
 
 const { form, loading, error, resetForm, saveUsuario } = useCrearUsuario();
 
@@ -19,6 +22,10 @@ const handleCrear = async () => {
             <Custom-input label="Usuario" placeholder="Ingrese el usuario" v-model="form.username" />
             <Custom-input label="Email" placeholder="Ingrese el email" v-model="form.email" />
             <Custom-input label="Password" placeholder="Ingrese una contraseña" v-model="form.password" />
+            <Custom-input label="Nombres" placeholder="Ingrese su nombre" v-model="form.firstName" />
+            <Custom-input label="Apellido" placeholder="Ingrese su apellido" v-model="form.lastName" />
+                  <Accordion title="Categoría" :text="form.categoria || 'Seleccione una opción'" :items="opciones"
+        @select="alSeleccionar" />
 
             <CustomButton :label="loading ? 'Cargando...' : 'Crear'" :disabled="loading" @click="handleCrear" />
             <p v-if="error" style="color: red; font-size: 12px;">{{ error }}</p>
